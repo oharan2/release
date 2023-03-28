90cdbfd7a45c834b6c223a18c6f97d49ce710acf
oharan2 <oharan@redhat.com>
2023-03-28 13:19:33 +0300

    Rosa get machine-type var from env with default

--- ci-operator/step-registry/msi/addon/test/rhods/msi-addon-test-rhods-commands.sh
+++ ci-operator/step-registry/msi/addon/test/rhods/msi-addon-test-rhods-commands.sh
@@ -11,3 +11,5 @@ OCM_TOKEN=$(cat /var/run/secrets/ci.openshift.io/cluster-profile/ocm-token)
 # TODO: understand how RHODS test should be ran from product QE image (quay.io/modh/ods-ci:latest)
 echo -e "cluster name: $CLUSTER_NAME\naddon name: $ADDON_NAME\napi host: $API_HOST\ntest marker: $TEST_MARKER"
 sleep 2h
+
+
